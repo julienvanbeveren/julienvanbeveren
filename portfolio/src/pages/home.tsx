80a6adedb730866e265379e6b5041753b13036da
julienvanbeveren <julien.vanbeveren@gmail.com>
2022-07-11 00:38:00 +0200

    added background animation

--- portfolio/src/pages/home.tsx
+++ portfolio/src/pages/home.tsx
@@ -1,18 +1,32 @@
-import React, { useEffect } from 'react'
-import { useFetch } from '@julienvanbeveren/hooks'
+import React, { useEffect, useState, useRef } from 'react'
+import { useFetch, useScript } from '@julienvanbeveren/hooks'
 import Project from '../components/Project'
-
+// @ts-ignore
+import GLOBE from 'vanta/dist/vanta.globe.min'
+    
 export default function Homepage() {
 
     const { loading, error, data } = useFetch<[]>('https://raw.githubusercontent.com/julienvanbeveren/julienvanbeveren/main/projects/projects.json')
 
-    if (data) {
-        console.log(data)
-    }
+    useEffect(() => {
+        GLOBE({
+            el: '#hero',
+            mouseControls: true,
+            touchControls: true,
+            gyroControls: false,
+            minHeight: 200.00,
+            minWidth: 200.00,
+            scale: 1.00,
+            scaleMobile: 1.00,
+            color: 0x1fa8ed,
+            color2: 0xffffff,
+            backgroundColor: 0x0f0f1c
+        })
+    }, [])
 
     return (
         <main>
-            <section>
+            <section id="hero">
                 <div className="container hero">
                     <div className="hero-wrapper">
                         <h1>Hello 👋, I am Julien</h1>
@@ -25,7 +39,7 @@ export default function Homepage() {
                     <h1 className="section-title">Projects</h1>
                     <div className="dividor"></div>
                     <div className="projects-grid">
-                        {data && data.splice(0,1).map((project, i) => {
+                        {data && data.map((project, i) => {
                             return <Project data={project} key={i}/>
                         })}
                     </div>
