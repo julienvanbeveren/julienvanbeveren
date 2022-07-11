d4e19446914c4a2a90510cbed0e43b25ad78e15c
julienvanbeveren <julien.vanbeveren@gmail.com>
2022-07-11 10:51:11 +0200

    added github to secretmessage

--- portfolio/src/components/Project.tsx
+++ portfolio/src/components/Project.tsx
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from 'react'
+import Github from '../assets/Github'
 
 interface ProjectProps {
-    data: {
-        date: number
-        github: string
-        ['long-description']: string
-        ['short-description']: string
-        name: string
-        preview: string
-        slug: string
-        status: 'Finished' | 'Active' | 'Discontinued'
-        tags: []
-        website: string
-    }
+    data: ProjectData
     delay: number
+}
+
+export interface ProjectData {
+    github: string
+    ['long-description']: string
+    ['short-description']: string
+    name: string
+    preview: string
+    slug: string
+    status: 'Finished' | 'Active' | 'Discontinued'
+    tags: []
+    website: string
 }
 
 export default function Project({ data, delay }: ProjectProps) {
@@ -30,6 +32,22 @@ export default function Project({ data, delay }: ProjectProps) {
         <div className={`project-card ${show ? 'visible' : 'hidden'}`}>
             <h1>{data.slug}</h1>
             <p>{data['short-description']}</p>
+            <ProjectLinks data={data}/>
+        </div>
+    )
+}
+
+interface ProjectLinksProps {
+    data: ProjectData
+}
+
+function ProjectLinks({ data }: ProjectLinksProps) {
+    
+
+
+    return (
+        <div className="project-links-wrapper">
+            {data.github && <Github />}
         </div>
     )
 }
